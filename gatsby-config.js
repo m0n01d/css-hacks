a35bd75f43a2bcd5fada04b825b91e79f9153df0
Dwight Doane <dwightdoane@me.com>
2019-10-16 17:41:03 -0400

    wip -- setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `CSS-Hacks`,
+    author: `Dwight Doane`,
+    description: `A place to share awesome frontend hacks and workarounds`,
+    siteUrl: `https://css-hacks.com`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `iamdwit`,
     },
   },
   plugins: [
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        //trackingId: `ADD YOUR TRACKING ID HERE`, // @todo
       },
     },
     `gatsby-plugin-feed`,
--- src/components/bio.js
+++ src/components/bio.js
@@ -23,16 +23,13 @@ const Bio = () => {
       }
       site {
         siteMetadata {
-          author
-          social {
-            twitter
-          }
+          description
         }
       }
     }
   `)
 
-  const { author, social } = data.site.siteMetadata
+  const { description } = data.site.siteMetadata
   return (
     <div
       style={{
@@ -40,27 +37,7 @@ const Bio = () => {
         marginBottom: rhythm(2.5),
       }}
     >
-      <Image
-        fixed={data.avatar.childImageSharp.fixed}
-        alt={author}
-        style={{
-          marginRight: rhythm(1 / 2),
-          marginBottom: 0,
-          minWidth: 50,
-          borderRadius: `100%`,
-        }}
-        imgStyle={{
-          borderRadius: `50%`,
-        }}
-      />
-      <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
-        {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
-        </a>
-      </p>
+      <p>{description}</p>
     </div>
   )
 }
