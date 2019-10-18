72152b8557423b150e5b06c82add0617eb376aa6
Dwight Doane <dwightdoane@me.com>
2019-10-17 22:18:10 -0400

    clean up typography

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
 
-// import { rhythm, scale } from "../utils/typography"
-
 class Layout extends React.Component {
   render() {
     const { location, title, children } = this.props
@@ -13,8 +11,7 @@ class Layout extends React.Component {
       header = (
         <h1
           style={{
-            // ...scale(1.5),
-            // marginBottom: rhythm(1.5),
+            marginBottom: "1rem",
             marginTop: 0,
           }}
         >
@@ -56,8 +53,7 @@ class Layout extends React.Component {
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
-          // maxWidth: rhythm(24),
-          // padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          padding: "1rem 1.75rem",
         }}
       >
         <header>{header}</header>
