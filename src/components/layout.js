74c9e4ffd6fe49a0e654432e39937c96b5797931
Dwight Doane <dwightdoane@me.com>
2019-10-21 19:41:00 -0400

    rm engine and add url to github to footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -58,9 +58,15 @@ class Layout extends React.Component {
         <header>{header}</header>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <p>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </p>
+          <p>
+            Contribute a post on{" "}
+            <a href="https://github.com/imdwit/css-hacks">github</a>
+          </p>
         </footer>
       </div>
     )
